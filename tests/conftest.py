fa366b87564122e8bf9b5210c45f03504514bb28
Umar116 <Ilnur16REGGY@gmail.com>
2024-05-06 11:43:50 +0700

    Добавлена фикстура авторизации в личном кабинете

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,25 @@
 import pytest
 from selenium import webdriver
 
+from pages.base_page import BasePage
+from pages.login_page import LoginPage
+
 
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+
+    options = webdriver.FirefoxOptions()
+    options.page_load_strategy = 'normal'
+    browser = webdriver.Firefox(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
+@pytest.fixture(scope="function")
+def authorization(browser):
+    base = BasePage(browser)
+    login = LoginPage(browser)
+    base.go_to('login')
+    login.login()
