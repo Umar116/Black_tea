1bdf10b4502b555746fa96c010ad79b2d55664a7
Umar116 <Ilnur16REGGY@gmail.com>
2024-05-06 11:43:07 +0700

    увеличил время ожидания элемента

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,13 @@
+from selenium.webdriver.support.wait import WebDriverWait
+
 from configure import base_url
 
 
 class BasePage:
 
-    def __init__(self, browser, timeout=10):
+    def __init__(self, browser, timeout=5000):
         self.browser = browser
-        self.browser.implicitly_wait(timeout)
+        self.wait = WebDriverWait(browser, timeout)
         self.url = base_url
 
     def go_to(self, path):
