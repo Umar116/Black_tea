13d9f41531e45d078a9eb90ab9a715e769e4665c
Umar116 <Ilnur16REGGY@gmail.com>
2024-05-29 14:09:32 +0700

    Добавил локаторы страницы юзеры и каталога

--- datas/locators.py
+++ datas/locators.py
@@ -12,7 +12,7 @@ last_name_l = (By.CSS_SELECTOR, 'input[name="lastName"]')
 phone_l = (By.CSS_SELECTOR, 'input[name="phone"]')
 email_l = (By.CSS_SELECTOR, 'input[name="email"]')
 deal_list_l = (By.CLASS_NAME,
-               "MuiTypography-root.MuiTypography-body2.MuiTypography-noWrap.css-fsltt8")
+               "MuiTypography-root.MuiTypography-body2.MuiTypography-noWrap.css-1n4xcnw")
 chat_field = (By.NAME, "msg")
 send_message = (By.CSS_SELECTOR, 'button[aria-label="Send"]')
 first_name_field_error = (By.XPATH, '//p[text()=\'"firstName" is required\']')
@@ -20,5 +20,23 @@ first_name_field_error = (By.XPATH, '//p[text()=\'"firstName" is required\']')
 search_customer_field = (By.CSS_SELECTOR, 'input[placeholder="Enter phone number, email, or name"]')
 deal_notification = (By.CSS_SELECTOR, 'div[class="MuiAlert-message css-1xsto0d"]')
 
-customer_name_in_customer_list = (By.CSS_SELECTOR,
-                                  'p[class="MuiTypography-root MuiTypography-body1 css-x63jhb"]')
+customer_name_in_customer_list = (By.CLASS_NAME,
+                                  'MuiTypography-root.MuiTypography-body1.css-1bbjwv7')
+
+users_button = (By.CSS_SELECTOR, 'a[href="/users"]')
+users_page_title = (By.CSS_SELECTOR, 'h4[class="MuiTypography-root MuiTypography-h4 css-6x0aek"]')
+users_input_field = (By.CSS_SELECTOR, 'input[type="text"]')
+users_date_list = (By.CSS_SELECTOR, 'div[class="MuiDataGrid-row MuiDataGrid-row--lastVisible"]')
+users_role = (By.CSS_SELECTOR, 'div[data-field="role"]')
+all_user_roles = (By.XPATH, '//p[text()="all roles"]')
+role_list_dropdown = lambda roles: (By.XPATH, f'//p[text()="{roles}"]')
+
+all_user_statuses = (By.XPATH, '//p[text()="all statuses"]')
+status_drop_down = lambda statuses: (By.XPATH, f'//p[text()="{statuses}"]')
+user_statuses = (By.CSS_SELECTOR, 'span[class="MuiChip-label MuiChip-labelMedium css-14vsv3w"]')
+
+catalog = (By.CSS_SELECTOR, 'a[href="/catalog"]')
+add_product_button = (By.CSS_SELECTOR, 'button[class="MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeMedium css-yvjl6n"]')
+add_category_button = (By.XPATH, '//span[text()="Category"]')
+input_category_name_field = (By.XPATH, "//input[href*=':r']")
+create_category_button = (By.XPATH, '//p[text()="Create"]')
